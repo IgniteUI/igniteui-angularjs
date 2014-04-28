94dec2299bdd9337bf85de8450e976e98931a9b3
Craig Shoemaker <craig@craigshoemaker.net>
2014-04-28 11:41:37 -0700

    Updated header & grid sample
    Separated the 'add product' UI from the table bound to the data source
    results.

--- samples/js/igniteui-sample.js
+++ samples/js/igniteui-sample.js
@@ -2,10 +2,8 @@
 
 /*
     The factories are created to provide data and services
-    to the controllers. If you want to see the controllers
-    scroll to the  '// controllers' section or search
-    for 'controllers'.
-
+    to the controllers. 
+    
     All data and controllers are included in this single file
     to ensure everything is loaded in the proper order. We're
     trying to keep things simple for now by not introducing a 
@@ -14,7 +12,135 @@
 
 var app = angular.module('igniteui-sample', ['igniteui-directives']);
 
-// factories
+// controllers
+app.controller('gridController',
+
+            ['$scope', 'northwind',
+    function ($scope,   northwind) {
+
+        $scope.northwind = northwind.data;
+
+        var createNewProduct = function () {
+            return {
+                ProductID: $scope.northwind.length + 1,
+                ProductName: null,
+                QuantityPerUnit: null,
+                UnitPrice: null
+            };
+        };
+
+        $scope.newProduct = createNewProduct();
+
+        $scope.deleteProduct = function (index) {
+            debugger;
+            $scope.northwind.splice(index, 1);
+        };
+
+        $scope.addProduct = function () {
+            
+            debugger;
+            
+            $scope.northwind.add($scope.newProduct);
+
+            $scope.newProduct = createNewProduct();
+        };
+
+    }]);
+
+app.controller('comboController',
+
+            ['$scope', 'northwind',
+    function ($scope,   northwind) {
+
+        $scope.northwind = northwind.data;
+
+        $scope.combo = {
+            value1: 20,
+            value2: "Chang"
+        };
+
+    }]);
+
+app.controller('dataChartController',
+
+            ['$scope', 'populationData',
+    function ($scope,   populationData) {
+
+        $scope.dataChart = populationData.data;
+
+    }]);
+
+app.controller('editorsController',
+
+            ['$scope',
+    function ($scope) {
+
+        $scope.editors = {
+            currency: 12.1,
+            date: new Date(),
+            editor: 'Infragistics',
+            mask: '134-134-134',
+            numeric: 123,
+            percent: 0.12,
+            text: 'Ignite UI'
+        };
+
+    }]);
+
+app.controller('treeController',
+
+            ['$scope', 'productCategories',
+    function ($scope,   productCategories) {
+
+        $scope.ProductCategories = productCategories.data;
+
+        $scope.deleteProductCategory = function (ix) {
+            $scope.ProductCategories.splice(ix, 1);
+        };
+
+        $scope.deleteProductSubcategory = function (pix, ix) {
+            $scope.ProductCategories[pix].ProductSubcategories.splice(ix, 1);
+        };
+
+        $scope.addProductCategory = function () {
+            var tmp = angular.copy($scope.newProductCategory);
+            $scope.ProductCategories.push(tmp);
+        };
+
+        $scope.addProductSubcategory = function (ix) {
+            var tmp = angular.copy($scope.newProductSubcategory);
+            $scope.ProductCategories[ix].ProductSubcategories.push(tmp);
+        };
+
+        $scope.newProductSubcategory = {
+            Name: "",
+            ProductSubcategoryID: 1,
+            Products: []
+        };
+
+        $scope.newProductCategory = {
+            Name: "",
+            ProductCategoryID: 1,
+            ProductSubcategories: []
+        };
+
+        $scope.addNodeRootLevel = function () {
+            $("#tree1").igTree("addNode", { Name: "New Node", ProductCategoryID: $scope.ProductCategories.length + 1 });
+        };
+
+        $scope.removeSelectedNode = function () {
+            var selected = $('#tree1').igTree('selectedNode');
+            if (selected.path !== null) {
+                $('#tree1').igTree('removeAt', selected.path);
+            } else {
+                alert('Select a node from the tree first');
+            }
+        };
+
+    }]);
+
+
+// data factories
 app.factory('northwind', [function () {
 
     var svc = {
@@ -2612,125 +2738,3 @@ app.factory('productCategories', [function () {
     return svc;
 
 }]);
-
-// controllers
-app.controller('gridController',
-
-            ['$scope', 'northwind',
-    function ($scope,   northwind) {
-
-        $scope.northwind = northwind.data;
-
-        var createNewProduct = function () {
-            return {
-                ProductName: null,
-                QuantityPerUnit: null,
-                UnitPrice: null
-            };
-        };
-
-        $scope.newProduct = createNewProduct();
-
-        $scope.deleteProduct = function (index) {
-            $scope.northwind.splice(index, 1);
-        };
-
-        $scope.addProduct = function () {
-            northwind.add($scope.newProduct);
-
-            $scope.newProduct = createNewProduct();
-        };
-
-    }]);
-
-app.controller('comboController',
-
-            ['$scope', 'northwind',
-    function ($scope,   northwind) {
-
-        $scope.northwind = northwind.data;
-
-        $scope.combo = {
-            value1: 20,
-            value2: "Chang"
-        };
-
-    }]);
-
-app.controller('dataChartController',
-
-            ['$scope', 'populationData',
-    function ($scope,   populationData) {
-
-        $scope.dataChart = populationData.data;
-
-    }]);
-
-app.controller('editorsController',
-
-            ['$scope',
-    function ($scope) {
-
-        $scope.editors = {
-            currency: 12.1,
-            date: new Date(),
-            editor: 'Infragistics',
-            mask: '134-134-134',
-            numeric: 123,
-            percent: 0.12,
-            text: 'Ignite UI'
-        };
-
-    }]);
-
-app.controller('treeController',
-
-            ['$scope', 'productCategories',
-    function ($scope,   productCategories) {
-
-        $scope.ProductCategories = productCategories.data;
-
-        $scope.deleteProductCategory = function (ix) {
-            $scope.ProductCategories.splice(ix, 1);
-        };
-
-        $scope.deleteProductSubcategory = function (pix, ix) {
-            $scope.ProductCategories[pix].ProductSubcategories.splice(ix, 1);
-        };
-
-        $scope.addProductCategory = function () {
-            var tmp = angular.copy($scope.newProductCategory);
-            $scope.ProductCategories.push(tmp);
-        };
-
-        $scope.addProductSubcategory = function (ix) {
-            var tmp = angular.copy($scope.newProductSubcategory);
-            $scope.ProductCategories[ix].ProductSubcategories.push(tmp);
-        };
-
-        $scope.newProductSubcategory = {
-            Name: "",
-            ProductSubcategoryID: 1,
-            Products: []
-        };
-
-        $scope.newProductCategory = {
-            Name: "",
-            ProductCategoryID: 1,
-            ProductSubcategories: []
-        };
-
-        $scope.addNodeRootLevel = function () {
-            $("#tree1").igTree("addNode", { Name: "New Node", ProductCategoryID: $scope.ProductCategories.length + 1 });
-        };
-
-        $scope.removeSelectedNode = function () {
-            var selected = $('#tree1').igTree('selectedNode');
-            if (selected.path !== null) {
-                $('#tree1').igTree('removeAt', selected.path);
-            } else {
-                alert('Select a node from the tree first');
-            }
-        };
-
-    }]);
