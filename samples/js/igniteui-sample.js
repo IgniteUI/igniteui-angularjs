1353e5ea1df371a14084606bfe26d401448ae1d8
damyanpetev <damyanpetev@users.noreply.github.com>
2014-06-02 13:04:02 +0300

    Grid with controller options sample

--- samples/js/igniteui-sample.js
+++ samples/js/igniteui-sample.js
@@ -45,6 +45,36 @@ app.controller('gridController',
             $scope.newProduct = createNewProduct();
         };
 
+        $scope.gridOptions = {
+            dataSource: $scope.northwind,
+            width: "100%",
+            height: "400px",
+            primaryKey: "ProductID",
+            autoCommit: true,
+            autoGenerateColumns: false,
+            columns: [
+                   { "headerText": "Product ID", "key": "ProductID", "dataType": "number", "width": "50px" },
+                   { "headerText": "Name", "key": "ProductName", "dataType": "string", "width": "250px" },
+                   { "headerText": "Quantity per unit", "key": "QuantityPerUnit", "dataType": "string", "width": "200px" },
+                   { "headerText": "Unit Price", "key": "UnitPrice", "dataType": "string", "width": "100px" }
+            ],
+            features: [{
+                name: "Updating",
+                columnSettings: [{
+                    columnKey: "ProductID",
+                    readOnly: true
+                }]
+            }, {
+                name: "Paging",
+                pageSize: 10
+            }, {
+                name: "Filtering"
+            }, {
+                name: "Sorting"
+            }]
+
+        };
+
     }]);
 
 app.controller('comboController',
