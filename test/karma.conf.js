c34032fe5df3a6c46712633db37422b76582ae14
kdinev <kdinev@mail.bw.edu>
2016-04-04 16:23:19 +0300

    Updating the repo to use latest version of deps.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,16 +4,16 @@ module.exports = function(config){
 		basePath : '../',
 
 		files : [
-		'http://code.jquery.com/jquery-1.10.2.min.js',
+			'http://code.jquery.com/jquery-1.10.2.min.js',
 			'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js',
 			'bower_components/angular/angular.js',
 			'bower_components/angular-route/angular-route.js',
 			'bower_components/angular-mocks/angular-mocks.js',
-		'http://cdn-na.infragistics.com/igniteui/latest/js/infragistics.core.js',
+			'http://cdn-na.infragistics.com/igniteui/latest/js/infragistics.core.js',
 			'http://cdn-na.infragistics.com/igniteui/latest/js/infragistics.dv.js',
 			'http://cdn-na.infragistics.com/igniteui/latest/js/infragistics.lob.js',
 			'src/igniteui-angular.js',
-		'test/app/js/my-app.js',
+			'test/app/js/my-app.js',
 			'test/unit/**/*.js'
 		],
 
@@ -23,12 +23,7 @@ module.exports = function(config){
 
 		browsers : ['Chrome'],
 
-		plugins : [
-						'karma-chrome-launcher',
-						'karma-firefox-launcher',
-						'karma-jasmine',
-						'karma-junit-reporter'
-						],
+		plugins : [ 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-jasmine', 'karma-junit-reporter' ],
 
 		junitReporter : {
 			outputFile: 'test_out/unit.xml',
