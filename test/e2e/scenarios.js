c4972f2eff671f160bf7a0e0b9203428e7470f29
Deyan Kamburov <DKamburov@infragistics.com>
2016-05-18 13:49:24 +0300

    Improve checking for multiple changes in scope

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -562,6 +562,15 @@ describe("my app", function() {
 			util.executeScript('angular.element("#grid1").scope().$apply();');
 			expect(util.getResult('$("#grid1_fixed tbody tr:eq(0) td")[0].innerHTML')).toBe("Tea");
 			expect(util.getResult('$("#grid1 tbody tr:eq(0) td")[0].innerHTML')).toBe("1");
+			util.executeScript('angular.element("#grid1").scope().northwind[1].ProductName = "Beer";');
+			util.executeScript('angular.element("#grid1").scope().northwind[2].UnitsOnOrder = 2;');
+			util.executeScript('angular.element("#grid1").scope().$apply();');
+			expect(util.getResult('$("#grid1_fixed tbody tr:eq(1) td")[0].innerHTML')).toBe("Beer");
+			//the other values should be preserved
+			expect(util.getResult('$("#grid1_fixed tbody tr:eq(2) td")[0].innerHTML')).toBe("Aniseed Syrup");
+			expect(util.getResult('$("#grid1 tbody tr:eq(2) td")[2].innerHTML')).toBe("2");
+			//the other values should be preserved
+			expect(util.getResult('$("#grid1 tbody tr:eq(1) td")[2].innerHTML')).toBe("24 - 12 oz bottles");
 		});
 	});
 	
